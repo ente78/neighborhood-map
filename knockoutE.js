aeb9fd1eee4371a0ec82da3a8a3f098430f3807b
ente78 <britta.ente@gmail.com>
2017-08-01 21:23:08 +0200

    ffff

--- knockoutE.js
+++ knockoutE.js
@@ -1,52 +1,112 @@
-const ViewModel = function(){
+
+// array with all 5 required places
+
+var places = [{
+        id: 'ChIJKyKVyeGOsUcRLG5WX_0h-84',
+        name: 'here i work',
+        location: {
+            lat: 53.554072,
+            lng: 10.003331,
+        },
+        type: 'work',
+   
+     
+    },
+    {
+        id: ' ChIJnZksd-GOsUcRXy8G81NGKoY',
+        name: 'nike',
+        location: {
+            lat: 53.552850,
+            lng: 10.004842,
+        },
+        type: 'shopping',
+       
+   
+    },
+    {
+        id: 'ChIJsZdNCeKOsUcReeo-KaQ5ed8',
+        name: 'perle',
+        location: {
+            lat: 53.551670,
+            lng: 9.999791, 
+        },
+        type: 'food',
+        
+             
+    },
+    {
+        id: 'ChIJKyKVyeGOsUcRLG5WX_0h-84',
+        name: 'jack jones ',
+        location: {
+            lat: 53.552315,
+            lng: 10.003437,
+        },
+        type: 'clothes',
+       
+ 
+
+        },
+    {
+        id: ' ChIJU7knXByPsUcR-Na5tai2l8g',
+        name: 'douglas',
+        location: {
+            lat: 53.552251,
+            lng: 10.003797, 
+        },
+        type: 'shopping',
+      
+
+    },
+
+];
+
+
+var place = function(data){
+    this.name = ko.observable(data.name);
+    this.id= ko.observable(data.id);
+    this.type = ko.observable(data.type); 
+    this.input = ko.observable("");
+    this.inputfilter = ko.observableArray([]);   
+    }; 
+
+
+var ViewModel = function(){
     var self = this;
     this.myplaces = ko.observableArray([]);
 
     places.forEach(function(placeitem){
-	self.myplaces.push(new place(placeitem));
+		self.myplaces.push(new place(placeitem));
         });
 
-/*function myFunction() {
-    // Declare variables
-    
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
+    this.input = ko.observable("");
+   
 
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}*/
-}
-	this.input = ko.computed(function(){
-		var search = this.search().toLowerCase();
-    	if (!search) {
-        	return null;
-    	} else {
-        	return ko.utils.arrayFirst(this.filteredItems(), function(item) {
-            	return ko.utils.stringStartsWith(item.name().toLowerCase(), search);
-        });
-	}; 
-	})
+    this.search = ko.computed(function(){
+	    var filter = self.input().toLowerCase();
+   		 return ko.utils.arrayFilter(places, function(marker) {
+   			 if (marker.title.toLowerCase().indexOf(filter) > -1) {
+         		 return marker.setVisible(true);
+     	 } else {
+         		 return marker.setVisible(false);
+    	  }
+   		 });
+	}, self);
+};
+
+/*
+
+If you wish to just to hide/show the marker you could use the setVisible method of of the marker like:
+
+ start_marker.setVisible(false);//to hide
+ start_marker.setVisible(true);//to show
+
+ */
 
 
-
-
-    var place = function(data){
-    this.name = ko.observable(data.name);
-    this.id= ko.observable(data.id);
-    this.type = ko.observable(data.type); 
-    }
 
 
 ko.applyBindings(new ViewModel()); 
+
+
 
 
