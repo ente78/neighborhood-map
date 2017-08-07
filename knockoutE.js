a92f92321007912928bea9968e0c80d28fbefd7c
ente78 <britta.ente@gmail.com>
2017-08-07 18:46:48 +0200

    asteta

--- knockoutE.js
+++ knockoutE.js
@@ -1,4 +1,110 @@
 
+var markers=[];
+
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 53.5528124, lng: 10.0047905},
+        zoom: 15        
+    });
+
+        // Style the markers a bit. This will be our listing marker icon.
+        var defaultIcon = makeMarkerIcon('0091ff');
+
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = makeMarkerIcon('FFFF24');
+
+
+     var largeInfowindow = new google.maps.InfoWindow();
+        var bounds = new google.maps.LatLngBounds();
+
+        // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < places.length; i++) {
+          // Get the position from the location array.
+          var position = places[i].location;
+
+          var title = places[i].name;
+          var id = places[i].id; 
+          console.log(title);
+          console.log(id);
+          console.log(position); 
+
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            icon: defaultIcon, 
+            animation: google.maps.Animation.DROP,
+            id: id
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow); 
+           });
+
+          marker.addListener('click', function(){
+            toggleBounce(this); 
+          }); 
+      
+          function toggleBounce(marker) {
+              marker.setAnimation(google.maps.Animation.BOUNCE);
+              setTimeout(function(){ marker.setAnimation(null); }, 750);
+      }
+
+
+        // Two event listeners - one for mouseover, one for mouseout,
+        // to change the colors back and forth.
+        marker.addListener('mouseover', function() {
+           this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+           this.setIcon(defaultIcon);
+          });
+
+
+          bounds.extend(markers[i].position);
+        }
+        // Extend the boundaries of the map for each marker
+        map.fitBounds(bounds);
+      }
+
+
+
+    // This function takes in a COLOR, and then creates a new marker
+    // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+      // of 0, 0 and be anchored at 10, 34).
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+          return markerImage;
+    }
+
+          // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title+ "  " + marker.id +'</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+
+          });
+    }; 
+  }
+
 // array with all 5 required places
 
 var places = [{
@@ -77,38 +183,11 @@ var ViewModel = function(){
     self.currentplace= ko.observable(self.myplaces()[0]); 
 	
 	self.markerbounce = function(id) {  
-		toggleBounce(places.id);  
+		this.toggleBounce(places.id);  
 	};
 
 
 }; 
-   // self.input = ko.observable("");
-
-   
-/*
-    this.search = ko.computed(function(){
-	    var filter = self.input().toLowerCase();
-   		 return ko.utils.arrayFilter(places, function(marker) {
-   			 if (marker.title.toLowerCase().indexOf(filter) > -1) {
-         		 return marker.setVisible(true);
-     	 } else {
-         		 return marker.setVisible(false);
-    	  }
-   		 });
-	}, this);
-*/ 
-
-
-
-/*
-
-If you wish to just to hide/show the marker you could use the setVisible method of of the marker like:
-
- start_marker.setVisible(false);//to hide
- start_marker.setVisible(true);//to show
-
- */
-
 ko.applyBindings(new ViewModel()); 
 
 
