4e4fa7d302f8dd84874cbc91b584ab96e50bf66b
ente78 <britta.ente@gmail.com>
2017-09-03 18:57:45 +0200

    last

--- knockoutE.js
+++ knockoutE.js
@@ -55,36 +55,37 @@ var places = [{
 
 ];
 
+
 var markers=[];
-
-
-
+var infowindow; 
+var defaultIcon;
+var highlightedIcon;
 
 function toggleBounce(marker) {
-    	marker.setAnimation(google.maps.Animation.BOUNCE);
-    	setTimeout(function(){ marker.setAnimation(null); }, 750);
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){ marker.setAnimation(null); }, 750);
     }  
 
 
 function initMap() {
- 	
+
 
      // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 53.5528124, lng: 10.0047905},
         zoom: 15        
     });
-	
+  
 
         // Style the markers a bit. This will be our listing marker icon.
-        var defaultIcon = makeMarkerIcon('0091ff');
+        defaultIcon = makeMarkerIcon('0091ff');
 
         // Create a "highlighted location" marker color for when the user
         // mouses over the marker.
-        var highlightedIcon = makeMarkerIcon('FFFF24');
+        highlightedIcon = makeMarkerIcon('FFFF24');
 
 
-     var largeInfowindow = new google.maps.InfoWindow();
+        infowindow = new google.maps.InfoWindow();
         var bounds = new google.maps.LatLngBounds();
 
         // The following group uses the location array to create an array of markers on initialize.
@@ -94,9 +95,7 @@ function initMap() {
 
           var title = places[i].name;
           var id = places[i].id; 
-          console.log(title);
-          console.log(id);
-          console.log(position); 
+
 
           // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
@@ -111,22 +110,15 @@ function initMap() {
           markers.push(marker);
           viewModel.myplaces()[i].marker= marker; 
           // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow); 
-           });
+          marker.addListener('click', populateInfoWindow); 
+           
 
-          marker.addListener('click', function(){
-            toggleBounce(this); 
-          }); 
 
         // Two event listeners - one for mouseover, one for mouseout,
         // to change the colors back and forth.
-        marker.addListener('mouseover', function() {
-           this.setIcon(highlightedIcon);
-        });
-        marker.addListener('mouseout', function() {
-           this.setIcon(defaultIcon);
-          });
+        marker.addListener('mouseover', highlight);
+        marker.addListener('mouseout', changecolor);
+          
 
 
           bounds.extend(markers[i].position);
@@ -134,9 +126,15 @@ function initMap() {
         // Extend the boundaries of the map for each marker
         map.fitBounds(bounds);
       }
+  function changecolor(){
+      this.setIcon(defaultIcon);
+  }
+
+  function highlight(){
+    this.setIcon(highlightedIcon);
 
 
-
+  }
     // This function takes in a COLOR, and then creates a new marker
     // icon of that color. The icon will be 21 px wide by 34 high, have an origin
       // of 0, 0 and be anchored at 10, 34).
@@ -155,63 +153,65 @@ function initMap() {
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
 
-      function populateInfoWindow(marker, infowindow,content) {
-      	getWiki(marker, infowindow); 
+      function populateInfoWindow() {
+        var marker = this; 
+        getWiki(marker, infowindow); 
+        toggleBounce(marker); 
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title+ "  " + content + '</div>');
-          infowindow.open(map, marker,content);
+          infowindow.setContent('<div>' + marker.title+ "  " + '</div>');
+          infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
 
           });
-    };
+    }
 }
    
    function getContent(data) {
-   		var contentString = '<div class ="infoWindow"><h4 class="title">'+ data.title + '</h4>'+ '<div>' + data.content + '</div></div>';
-   		return contentString ;    	
+      var contentString = '<div class ="infoWindow"><h4 class="title">'+ data.title + '</h4>'+ '<div>' + data.content + '</div></div>';
+      return contentString ;      
    }
  
  // from the ajax course. this function shall get the wiki articles for the marker.  
-		function getWiki(marker, infowindow){
+    function getWiki(marker, infowindow){
 
-			var content = "<ul>"; 
-  			var wikiUrl = 'http://de.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+      var content = "<ul>"; 
+        var wikiUrl = 'http://de.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
 
-  		 var wikiRequestTimeout = setTimeout(function(){
-     	  	alert("failed to get wiki");
-   		 },8000);
+       var wikiRequestTimeout = setTimeout(function(){
+          alert("failed to get wiki");
+       },8000);
 
-   		 $.ajax({
-    		 url : wikiUrl, 
-       		 dataType: "jsonp",
-       		 // sjonp: "callback"
-        		success: function (response){
-            		var articleList = response [1];
+       $.ajax({
+         url : wikiUrl, 
+           dataType: "jsonp",
+           // sjonp: "callback"
+            success: function (response){
+                var articleList = response [1];
 
-            		for (var i = 0; i < articleList.length; i++) {
-              		  	articleStr = articleList [i];
-               			var url = 'http://de.wikipedia.org/wiki/' + articleStr;
-               		 	content+= ('<li><a href="' + url + '">' + articleStr + '</a></li>'); 
-           		 }
+                for (var i = 0; i < articleList.length; i++) {
+                      articleStr = articleList [i];
+                    var url = 'http://de.wikipedia.org/wiki/' + articleStr;
+                    content+= ('<li><a href="' + url + '">' + articleStr + '</a></li>'); 
+               }
 
-           			clearTimeout(wikiRequestTimeout); 
-						
-					var data = {
-						title: marker.title,
-						content: content        					
-       	 			} ;
+                clearTimeout(wikiRequestTimeout); 
+            
+          var data = {
+            title: marker.title,
+            content: content                  
+              } ;
 
-       	 			infowindow.setContent(getContent(data));
-       	 			infowindow.open (map, marker); 
-				}
+              infowindow.setContent(getContent(data));
+              infowindow.open (map, marker); 
+        }
 
-   	 });  
-   	 return false; 
-   	 }
+     });  
+     return false; 
+     }
 
 
 
@@ -221,45 +221,43 @@ var place = function(data){
     this.name = (data.name);
     this.id = (data.id);
     this.type = ko.observable(data.type); 
-    //this.input = ko.observable("");
-    //this marker =ko.observableArray([data.marker]); 
-    //this.inputfilter = ko.observableArray([]);   
     }; 
 
 
 var ViewModel = function(){
     var self = this;
     self.myplaces = ko.observableArray([]);
-    self.input = ko.observable("");
-    //this marker =ko.observableArray([data.marker]); 
+    self.input = ko.observable("");   
     self.inputfilter = ko.observableArray([]); 
 
     places.forEach(function(placeitem){
-		self.myplaces.push(new place(placeitem));
+    self.myplaces.push(new place(placeitem));
     });
     
     self.currentplace= ko.observable(self.myplaces()[0]); 
-	
-	self.markerbounce = function(place) {  
-		console.log(place)
-		toggleBounce(markers[place.id]);
-	};
+  
+  self.markerbounce = function(place) {  
+    console.log(place);
+    toggleBounce(markers[place.id]);
+    google.maps.event.trigger(place.marker,'click');
 
-	
+  };
+
+  
     this.search = ko.computed(function(){
-	    var filter = self.input().toLowerCase();
-	   // if (filter) 
-   		 return ko.utils.arrayFilter(self.myplaces(), function(place) {
-   			 var match= (place.name.toLowerCase().indexOf(filter) > -1) 
-   			 console.log(place); 
-     		 if (place.marker) {
-     		 place.marker.setVisible(match) 
-     		}; 
-     		 return match  		    
-    	  
+      var filter = self.input().toLowerCase();
+     // if (filter) 
+       return ko.utils.arrayFilter(self.myplaces(), function(place) {
+         var match= (place.name.toLowerCase().indexOf(filter) > -1) ;
+         
+         if (place.marker) {
+         place.marker.setVisible(match) ;
+        }
+         return match;     
+        
 
-   		 });
-	}, this);
+       });
+  }, this);
 
  
 
